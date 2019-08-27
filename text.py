d1eab95579da0276a0c5cb31a968abca215b4754
HenryChen1 <337865067@qq.com>
2019-08-27 11:28:20 +0800

    Update text.py

--- text.py
+++ text.py
@@ -14,7 +14,7 @@ class DownloadHanlder(Thread):
     def run(self):
         filename = self.url[self.url.rfind('/') + 1:]
         resp = requests.get(self.url)
-        with open('D:\py project\my-practice-code' + filename, 'wb') as f:
+        with open('D:\py project\pic\ ' + filename, 'wb') as f:
             f.write(resp.content)
 
 
@@ -24,13 +24,13 @@ def main():
     # 要使用该数据接口需要在天行数据的网站上注册
     # 然后用自己的Key替换掉下面代码的中APIKey即可
     resp = requests.get(
-        'http://api.tianapi.com/meinv/?key=ee1047c596271b2e92ec482cbadb9fb6&num=10')
+        'http://api.tianapi.com/txapi/saylove/?key=ee1047c596271b2e92ec482cbadb9fb6&num=20')
     # 将服务器返回的JSON格式的数据解析为字典
     data_model = resp.json()
     for mm_dict in data_model['newslist']:
-        url = mm_dict['picUrl']
+        url = mm_dict['content']
         # 通过多线程的方式实现图片下载
-        DownloadHanlder(url).start()
+        # DownloadHanlder(url).start()
 
 
 if __name__ == '__main__':
